61d119cf210326ad609920818c6c299854820963
Jacke <stateloader@protonmail.com>
2022-06-01 21:29:33 +0200

    well, learning git

--- network_project/client/client.c
+++ network_project/client/client.c
@@ -6,37 +6,17 @@
 int main(void) {
 
   sess_t session = session_setup("192.168.0.101", 2000);
-  
+
   char server_message[2000], client_message[MAX_COMMAND];
 
   memset(server_message,'\0',sizeof(server_message));
   memset(client_message,'\0',sizeof(client_message));
 
-<<<<<<< HEAD
-  // Create socket:
-  socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-
-  if(socket_desc < 0){
-      printf("Unable to create socket\n");
-      return -1;
-  }
-
-  printf("Socket created successfully\n");
-
-  // Set port and IP the same as server-side:
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(2000);
-  server_addr.sin_addr.s_addr = inet_addr("192.168.0.101");
-
-  // Send connection request to server:
-  if (connect(socket_desc, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
-=======
   if (connect(session.socket_desc, (struct sockaddr *)&session.server_addr, sizeof(session.server_addr)) < 0){
->>>>>>> 3d616b43c84642e226aeed01d3f2b12b0cd87f98
       printf("Unable to connect\n");
       return -1;
   }
-  
+
   printf("Connected with server successfully\n");
 
   scan_t scan = scan_driver("enter message");
--- network_project/client/src/session/session.c
+++ network_project/client/src/session/session.c
@@ -7,10 +7,9 @@ sess_t session_setup(char *address, int port) {
   session.server_addr.sin_family = AF_INET;
   session.server_addr.sin_port = htons(port);
   session.server_addr.sin_addr.s_addr = inet_addr(address);
-  
+
   session.socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-  
-  
+
   return session;
 }
 
