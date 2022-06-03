e6280a40790646c70e9a2cffb3894405754fb35f
Jacke <stateloader@protonmail.com>
2022-06-03 14:23:59 +0200

    s

--- network_project/client/src/command.c
+++ network_project/client/src/command.c
@@ -1,1 +1,41 @@
+#include "scan.h"
 #include "command.h"
+
+uint8_t static ENTRY = 0x00;
+
+static const char *BEGIN_CMND[] = {
+  "-fetch data", "-steer device", "-quit"
+};
+static const char *FETCH_CMND[] = {
+  "-temperature", "humidity", "-back"
+};
+static const char *STEER_CMND[] = {
+  "-red", "-blue", "-green", "-back"
+};
+
+static uint8_t begin_cmnd(char *command) {
+
+  scan = scan_driver("choose entry");
+}
+
+static void fetch_cmnd(char *command) {
+
+}
+
+static void steer_cmnd(char *command) {
+
+}
+
+typedef void (*cmnd_func)(char *command);
+
+typedef struct {
+  cmnd_func func;
+} cmnd_item;
+
+static cmnd_item cmnd_items[] = {
+  {begin_cmnd}, {fetch_cmnd}, {steer_cmnd}
+};
+
+void command_driver(size_t size, char *command) {
+
+}
--- network_project/client/src/command.h
+++ network_project/client/src/command.h
@@ -3,6 +3,18 @@
 
 #include "utils.h"
 
+#define ARRNOW 3
+
+#define BEGIN_ENTRY 0
+#define FETCH_ENTRY 1
+#define STEER_ENTRY 2
+
+typedef void (*cmnd_func)(char *command);
+
+typedef struct {
+  cmnd_func func;
+} cmnd_item;
+
 void command_driver(char *command, size_t size);
 
 #endif
