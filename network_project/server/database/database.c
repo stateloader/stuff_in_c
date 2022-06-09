ed08ac49ce395d40617c6879770302200c62d4dd
jacke <stateloader@protonmail.com>
2022-06-09 19:24:45 +0200

    void pointers don't play along atm

--- network_project/server/database/database.c
+++ network_project/server/database/database.c
@@ -1,7 +1,7 @@
 #include "database.h"
 #include "filedriver.h"
 
-static uint8_t route = 0x00;
+static uint8_t request = 0;
 
 /*-----------------------------------------------------------------------------------------------------------------------
                                                                                                                ROUTE BYTE
@@ -12,33 +12,14 @@ static uint8_t route = 0x00;
 
 ------------------------------------------------------------------------------------------------------------------------*/
 
-table_item table_items[] = {
-
-  {MCLNT, CCLNT, "resources/client.txt"},
-  {MSMPL, CSMPL, "resources/sample.txt"},
-  {MMSGE, CMSGE, "resources/message.txt"},
-};
-
-static void encode_byte(uint8_t request, filed_t *driver) {
-  
-  uint8_t check_bit = 0;
-  while (check_bit < 7) {
-    if (request & 0x01)
-      driver->item = table_items[check_bit];
-    check_bit++;
-    request = request >> 1;
-  }
-  driver->route = (request & (1 << 7) ? WINIT: RINIT);
-}
-
-
 int main(void) {
-  uint8_t request = 0;
-  request |= (1 << 7) | (1 << MMSGE);
-  filed_t driver = {.route = route};
-  encode_byte(request, &driver);
-  printf("UTANFÖR LOOP: item idx =  %s\n", driver.item.file_path);
-  uint8_t result = file_driver(&driver);
-  //free_driver(&driver);
+
+  uint8_t result = 0;
+  filed_t driver = {.route = 0x00};
+  request |= (0 << 7) | (1 << MCLNT);
+
+  result = file_driver(request, &driver);
+  printf("test result: %d\n", result);
+  free_driver(&driver);
   return 0;
 }
